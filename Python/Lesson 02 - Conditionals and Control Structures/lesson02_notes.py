021021e8c6edfa9eb0ba1bb16c7d9edf6988d1da
Walters Jose J. <jjw21060@gmail.com>
2026-07-15 10:11:04 -0400

    cd

---
+++ Python/Lesson 02 - Conditionals and Control Structures/lesson02_notes.py
@@ -0,0 +1,56 @@
+# user_number = input("Enter a number: ")
+# if int(user_number) >= 1:
+#     print("positive")
+# elif int(user_number) == 0:
+#     print("Zero")
+# else:
+#     print("negative")
+
+# user_number = input("Enter a number: ")
+# if int(user_number) >= 1:
+#     print("The number is positive")
+# else:
+#     print("Not positive")
+
+# user_number = input("Enter a number: ")
+# if int(user_number) % 2 == 0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+# user_number = input("Enter a number: ")
+# if int(user_number) <= 13:
+#     print("Child")
+# elif int(user_number) <= 19:
+#     print("Teenager")
+# elif int(user_number) <= 64:
+#     print("Adult")
+# else:
+#     print("Senior")
+
+# a = 10
+# b = 20
+# if a > b:
+#     print("Larger")
+# elif a < b:
+#     print("Smaller")
+# else:
+#     print("a is equal to b")
+
+# user_grade = input("Enter your grade: ")
+# if int(user_grade) >= 90:
+#     print("A")
+# elif int(user_grade) >= 80:
+#     print("B")
+# elif int(user_grade) >= 70:
+#     print("C")
+# elif int(user_grade) >= 60:
+#     print("D")
+# else:
+#     print("F")
+
+user_text = input("Enter your input string:  ")
+if len(user_text) > 10:
+    print("Long string")
+else:
+    print("Short string")
